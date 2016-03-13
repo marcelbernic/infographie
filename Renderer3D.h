6fb072d5b5203f07e11c392eb810e0c39dd9116c
Jesus_Soldierz <Jesus_SoldierZ@hotmail.com>
2016-03-13 14:10:26 -0400

    ajout sphere final

---
+++ Renderer3D.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "ofParameterGroup.h"
+#include "ofParameter.h"
+#include "ofApp.h"
+#include "Obj3D.h"
+#include "Cube3D.h"
+#include "Sphere3D.h"
+
+//class Renderer3D;
+class ofApp;
+
+/*template<typename ParameterType>
+class ofRenderer3DParam : public ofReadOnlyParameter<ParameterType, Renderer3D> {
+    friend class Renderer3D;
+};*/
+class Renderer3D
+{
+private:
+    ofApp * m_app;
+public:
+    Renderer3D();
+    ~Renderer3D();
+
+    void setup(const string p_name, ofApp *p_app);
+
+    void draw();
+
+    void imageExport(const string path, const string extension) const;
+
+    void xChanged(int & p_x);
+    void yChanged(int & p_y);
+    void zChanged(int & p_z);
+
+    void rotxChanged(double & p_rotx);
+    void rotyChanged(double & p_roty);
+    void rotzChanged(double & p_rotz);
+
+    void bCloudChanged(bool &p_cloud);
+
+    void drawCube3D(app::Cube3D *p_cube);
+
+    void drawSphere3D(app::Sphere3D *p_sphere);
+
+    ofParameterGroup parameters3D;
+    ofParameter<int> x;
+    ofParameter<int> y;
+    ofParameter<int> z;
+
+    ofParameter<double> rotx;
+    ofParameter<double> roty;
+    ofParameter<double> rotz;
+
+    ofParameter<bool> bCloud;
+};
